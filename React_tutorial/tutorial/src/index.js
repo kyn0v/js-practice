b4456c675b12131d1f08a5644060a1cbed0f1ab2
kynov <kynov@outlook.com>
2021-11-16 23:22:55 +0800

    lift up the state from individual Square to Board

--- React_tutorial/tutorial/src/index.js
+++ React_tutorial/tutorial/src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={() => this.props.onClick}>
-                {this.state.value}
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
@@ -20,17 +22,17 @@ class Board extends React.Component {
         };
     }
 
-    handleClick(i) {
-        /*TODO*/
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
     }
 
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.state.square[i]}
-                onClick={() => this.handleClick(i)}
-            />
-        );
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = "X";
+        this.setState({ squares: squares })
     }
 
     render() {
